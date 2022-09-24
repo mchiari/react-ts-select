959f1c161afdf629cdec29786cc9f3dcc4ed4a47
Matheus Chiari <mchiaridev@gmail.com>
2022-09-24 13:44:11 -0300

    finishing mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import './App.css'
-import { Select } from './Select'
-
+import { useState } from "react";
+import { Select, SelectOption } from "./Select";
 
 const options = [
-  {label: 'First', value: 1 },
-  {label: 'Second', value: 2 },
-  {label: 'Third', value: 3 },
-  {label: 'Fourth', value: 4 },
-  {label: 'Fifth', value: 5 },
-]
+	{ label: "First", value: 1 },
+	{ label: "Second", value: 2 },
+	{ label: "Third", value: 3 },
+	{ label: "Fourth", value: 4 },
+	{ label: "Fifth", value: 5 },
+	{ label: "Sixth", value: 6 },
+];
 
 function App() {
+	const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+	const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
 
-
-  return (
-    <div className="App">
-     <Select options={options} />
-    </div>
-  )
+	return (
+		<div className='App'>
+			<Select
+				multiple
+				value={value1}
+				options={options}
+				onChange={(o) => setValue1(o)}
+			/>
+			<br />
+			<Select
+				value={value2}
+				options={options}
+				onChange={(o) => setValue2(o)}
+			/>
+		</div>
+	);
 }
 
-export default App
+export default App;
